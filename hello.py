7da3f523b1ad7d98c0bdee7f10043e5f856d943c
花畑圭佑 <hanahatakeisuke@KHsMBP.local>
2019-07-05 13:31:19 +0900

    add second message

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 message = input('Please input message:')
 output = 'Hello ' + message + '!'
 print(output)
+message = input('Please input second message:')
+output = 'Hello ' + message + '!!'
+print(output)
